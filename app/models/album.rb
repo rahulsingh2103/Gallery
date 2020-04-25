9bbefd54ef9f8dfe12efb0af95d08608f1224e1c
rahulsingh2103 <rahulsingh2103@yahoo.com>
2020-04-25 22:58:05 +0530

    commit

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,12 +1,11 @@
 class Album < ApplicationRecord
-    paginates_per 2
+    paginates_per 3
     belongs_to :user
     has_many_attached :images
-  
 
     validates :title, presence: true
     validates :discription, presence: true
-    #validates :image_type 
+    validates :images, presence: true 
 
     def thumbnail input
         return self.images[input].variant(resize: '300x400!').processed
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,7 +1,4 @@
 class Home < ApplicationRecord
 
-    def thumbnail input
-        return self.images[input].variant(resize: '300x400!').processed
-    end
-
+   
 end
